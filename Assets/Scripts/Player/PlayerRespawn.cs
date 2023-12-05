eb18b312461e7e947bf4c916928d2c0db0586012
sarp <sarp@sarps-MacBook-Pro.local>
2023-12-05 20:46:20 +0300

    Player Life System Added

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -1,29 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerRespawn : MonoBehaviour
 {
     Animator animator;
+    [SerializeField] private GameObject[] hearts;
+
+    int life = 4;
 
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        life = hearts.Length;
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void playerHasDamage()
     {
         animator.SetTrigger("Hit");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        life--;
+        updateHealth();
+    }
+
+    public void updateHealth()
+    {
+        if(life==0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        else  if (life<1)
+        {
+            int lastIndex = hearts.Length - 1;
+            Destroy(hearts[lastIndex]);
+            hearts = hearts.Take(lastIndex).ToArray();
+            
+        }
+        else if (life<2)
+        {
+            int lastIndex = hearts.Length - 1;
+            Destroy(hearts[lastIndex]);
+            hearts = hearts.Take(lastIndex).ToArray();
+        }
+        else if (life<3)
+        {
+            int lastIndex = hearts.Length - 1;
+            Destroy(hearts[lastIndex]);
+            hearts = hearts.Take(lastIndex).ToArray();
+        }
+
     }
 }
